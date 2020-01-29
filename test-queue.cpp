982bbf0be353a0ea5634caa1e91f54cb9c7145cc
sharonhe98 <36315984+sharonhe98@users.noreply.github.com>
2020-01-28 21:45:44 -0500

    [ADD] Add tests for empty queues
    - Add a test that empty queues are equal to each other
    - Add a test showing popping from an empty queue does not change the size

--- test-queue.cpp
+++ test-queue.cpp
@@ -33,6 +33,7 @@ void queue_equality() {
   String* s2 = new String("two");
   Queue* q1 = new Queue();
   Queue* q2 = new Queue();
+  t_true(q1->equals(q2), "empty queues should be equal")
   q1->push(s1);
   t_true(!q1->equals(q2), "q1 should not equal q2");
   q2->push(s1);
@@ -107,6 +108,12 @@ void queue_pop_decrease_size() {
   t_true(q1->size() == 4, "size decreases to 4");
   q1->pop();
   t_true(q1->size() == 3, "size decreases to 3");
+  
+  q->pop();
+  q->pop();
+  q->pop();
+  q->pop();
+  t_true(q1->size() == 0, "size remains at 0");
 
   OK("queue pop decrease size");
 
