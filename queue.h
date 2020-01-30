68e37f54ca60fdf02b0100aed72bfdc6c106034d
Max Cruz <cruz.max@husky.neu.edu>
2020-01-29 20:05:42 -0500

    making queue methods virtual; removing a constructor

--- queue.h
+++ queue.h
@@ -13,38 +13,35 @@ class Queue : public Object {
     // constructs an empty Queue
     Queue();
 
-    // constructs an empty Queue of a desired initial size
-    Queue(size_t size);
-
     // frees the Queue from memory
     virtual ~Queue();
 
     // determines if this Queue is equal to the given Object o
-    bool equals(Object* o);
+    virtual bool equals(Object* o);
 
     // computes a hash for this Queue
-    size_t hash();
+    virtual size_t hash();
 
     // pushes the given Object to the end of the Queue
-    void push(Object* o);
+    virtual void push(Object* o);
 
     // removes and returns the Object at the front of the Queue if one exists
     // else returns nullptr
-    Object* pop();
+    virtual Object* pop();
 
     // returns the Object at the front of the Queue, but does not remove it, if one exists
     // else returns nullptr
-    Object* peek();
+    virtual Object* peek();
 
     // clears the elements in this Queue
-    void clear();
+    virtual void clear();
 
     // determines if this Queue is empty
-    bool is_empty();
+    virtual bool is_empty();
 
     // returns the number of elements in this Queue
-    size_t size();
+    virtual size_t size();
 
     // returns a string representation of this Queue (optional)
-    char* to_string();
+    virtual char* to_string();
 };
